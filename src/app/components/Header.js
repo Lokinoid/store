39beb9425e603c65c7987ef4a1d39860fb9da50c
Stanislav Orlov <stanor@microsoft.com>
2017-05-13 15:43:28 +0200

    Header implementation and style change

---
+++ src/app/components/Header.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+
+import {blueGrey50, blueGrey900} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+
+const muiTheme = getMuiTheme({
+    palette: {
+        accent1Color: blueGrey50,
+    },
+});
+
+const iconElemen = 
+    <IconButton>
+        <NavigationMenu
+            color={blueGrey900}
+        />
+    </IconButton>
+                
+class Header extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.title = 'Dancing shoes';
+    }
+
+    render() {
+        return (
+            <AppBar
+                title={this.title}
+                style={{backgroundColor: blueGrey50}}
+                iconElementLeft={iconElemen}
+                titleStyle={{color: blueGrey900}}
+            />
+        );
+    }
+}
+
+export default Header;
